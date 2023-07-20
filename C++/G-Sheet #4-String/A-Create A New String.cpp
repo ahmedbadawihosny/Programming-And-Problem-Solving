cb03dd0573b6986e50f0df22a41a4faf21168341
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-20 18:42:18 +0300

    Add files via upload

---
+++ C++/G-Sheet #4-String/A-Create A New String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string a,b;
+  cin>>a>>b;
+  cout<<a.size()<<" "<<b.size()<<endl;
+  cout<<a<<" "<<b<<endl;
+    return 0;
+}
