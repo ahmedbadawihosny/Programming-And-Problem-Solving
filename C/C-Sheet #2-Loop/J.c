a3d26fe83812421428618ca69b9412a076c1e038
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:27:11 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/J.c
+++ C/C-Sheet #2-Loop/J.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-int main() {
-
+  int main() {
+  int num;
+  scanf("%d", &num);
+  for (int i = 2;i <= num;i++){
+      int y = 0;
+      for (int x = 2 ;x < i;x++){
+        if (i % x == 0)
+            y = 1;
+      }
+        if (y == 0){
+            printf("%d ",i);
+        }
+  }
   return 0;
-}
+  }
