5f2147068d0cbd3bfd2f99bcc768524bf609926d
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:28:26 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/N.c
+++ C/C-Sheet #2-Loop/N.c
@@ -1,7 +1,18 @@
-
 #include <stdio.h>
 
 int main() {
+  char opr;
+  scanf("%c", &opr);
+  int lines;
+  scanf("%d", &lines);
+  for (int i = 1 ; i <= lines ;i++){
+      int x;
+      scanf("%d", &x);
+      while (x--){
+        printf("%c", opr);
+      }
+        printf("\n");
+  }
 
   return 0;
 }
