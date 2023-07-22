d10d8e95d2e335059e6344d7aca7ab46d50755b1
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:30:58 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/V.c
+++ C/C-Sheet #2-Loop/V.c
@@ -1,7 +1,24 @@
-
 #include <stdio.h>
 
 int main() {
+  int num;
+  scanf("%d", &num);
+  int counter = 1;
+  for (int i = 1;i <= num;i++){
+      if (counter % 4 == 0){
+        counter++;
+        printf("PUM\n");
+        i--;
+        continue;
+      }
+      printf("%d ",counter);
+      counter++;
+      printf("%d ",counter);
+      counter++;
+      printf("%d ",counter);
+      counter++;
+    }
+      printf("PUM\n");
 
   return 0;
 }
