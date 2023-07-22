2381e40b2ff4724cc01d19f68efdaef424998810
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:26:09 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/G.c
+++ C/C-Sheet #2-Loop/G.c
@@ -1,7 +1,17 @@
-
 #include <stdio.h>
 
 int main() {
-
-  return 0;
+  int t;
+  scanf("%d", &t);
+  long long fac =1 ;
+  while(t--){
+      int num;
+      scanf("%d", &num);
+      for(int i = 1 ; i <= num ; i++){
+          fac *= i;
+      }
+      printf("%lld\n", fac);
+      fac = 1;
+  }
+return 0;
 }
