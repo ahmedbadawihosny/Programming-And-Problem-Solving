400286fffd59a430a51cf7ebece4189a7c43a65e
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:28:05 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/M.c
+++ C/C-Sheet #2-Loop/M.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
+  int a,b;
+  int counter = -1;
+  scanf("%d %d",&a,&b);
+  for (int i = a;i <= b;i++ ){
+      int x = 0;
+      int j = i;
+      while (j){
+          if (j % 10 != 4 && j % 10 != 7)
+              x++;
+              j = j / 10;
+      }
+      if (x == 0){
+          printf("%d ",i);
+          counter++;
+      }
+  }
+    if (counter == -1){
+      printf("-1\n");
+    }
 
-  return 0;
+return 0;
 }
