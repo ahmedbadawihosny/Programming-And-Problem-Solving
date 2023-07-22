a109db3567bd26aa9a09bac2257d3edf8a16c0d5
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:29:02 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/P.c
+++ C/C-Sheet #2-Loop/P.c
@@ -1,7 +1,14 @@
-
 #include <stdio.h>
 
 int main() {
+  int num ;
+  scanf("%d", &num);
+  for (int i = 1;i <= num;i++){
+    for (int x = num ;x >= i;x--){
+      printf("*");
+    }
+      printf("\n");
+  }
 
   return 0;
 }
