2ad94df42be609eff3a4bc4700a159047b5b3e4f
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:24:25 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/B.c
+++ C/C-Sheet #2-Loop/B.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-
+  int counter;
+  scanf("%d", &counter);
+  if(counter == 1){
+    printf("-1");
     return 0;
+  }
+  for(int i = 1 ; i <=counter ; i++){
+    if(i % 2 == 0){
+        printf("%d\n",i);
+    }
+  }
+
+  return 0;
 }
