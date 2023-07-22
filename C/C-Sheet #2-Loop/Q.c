b9db9514b9670682b8c0ce91f60e20fe6d204443
Ahmed Badawi <119693125+ahmedbad010@users.noreply.github.com>
2023-07-22 17:29:18 +0300

    upload sheet#2 folder

--- C/C-Sheet #2-Loop/Q.c
+++ C/C-Sheet #2-Loop/Q.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
+int main(){
+  long long t,n;
+  scanf("%lld",&t);
+  for(long long i=1;i<=t;i++){
+    scanf("%lld",&n);
+    if (n==0){
+        printf("0");
+    }
+    else{
+    while (n>0){
+        long long first=0;
+        first =(n%10);
+        n/=10;
+        printf("%lld ",first);
+    }
+    }
+    printf("\n");
+  }
 
   return 0;
 }
